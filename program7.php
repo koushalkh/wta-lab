c3818c228b294374dcc86ede0e940606e9de561c
Koushal <koushalkh@gmail.com>
2018-12-09 23:02:40 +0530

    Update program7.php

--- program7.php
+++ program7.php
@@ -2,3 +2,23 @@
 date_dafault_timezone_set("Asia/Kolkata");
 echo date ("d/m/y::h:i:s A");
 ?>
+<!DOCTYPE HTML> 
+<html>
+<head>
+    <meta http-equiv="refresh" content="1"/> 
+<style>
+    p{
+        color:white;
+        font-size:90px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    body {
+        background-color:black;
+    } 
+</style>
+<p> <?php echo date(" h: i : s A");?> </p> 
+</head>
+</html>
